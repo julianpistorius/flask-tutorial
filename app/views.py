4b7d29202dd90016492cfbd86994d2be4b97c501
Adam Soll <solladame@gmail.com>
2014-07-25 14:39:08 -0700

    part 2

--- app/views.py
+++ app/views.py
@@ -1,7 +1,23 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello World!"
+    user = { 'nickname': 'Adam' }   # fake user
+    posts = [   # fake array of posts
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'How do I use flask?'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'Is this going to appear on the web page?'
+        }
+    ]
+
+    return render_template("index.html",
+        title = 'Home',
+        user = user,
+        posts = posts)
